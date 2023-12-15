acb3ca0ec7ca3aac1dfcb74446f33ca398ee59b1
Aminemazari <mazariamine095@gmail.com>
2023-12-15 02:13:08 +0100

    i push the singin page without image and not lot of style

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-)
-
-
-<<<<<<< HEAD
-=======
-
 ReactDOM.createRoot( document.getElementById('root') ).render(
-   
+<App/>   
 )
->>>>>>> b31dd83061818443a333544be9933c3230e4da12
+
+
--- Client/src/pages/singIn/singInPage.jsx
+++ Client/src/pages/singIn/singInPage.jsx
@@ -1,30 +1,22 @@
 import React from 'react'
-import Form from './form'
-const singIn = () => {
+import "./style/logInpage.css"
+import LogInForm from "./form"
+const SingInPage = () => {
   return (
-    <div class="split-container">
-      
-    <div class="image-side">
-      <img  className="TheX" src={TheX}></img>
+    <div>
+      <div class="split-container"> 
+      <div class="image-side">
 
-      <h1 className='hero-title'>Hear from experienced individuals</h1>
-      <h1 className='hero-title1'>LOGO</h1>
-    </div>
-    <div class="color-side">
-      <div className='LogInForm'> 
-    <Form></Form>
-    </div>
-    <img  className="Sagittarius" src={Sagittarius}></img>
-      <img  className="Vector" src={Vector}></img>
-      
-    </div>
-
-</div>
+        <h1 className='hero-title'>Hear from experienced individuals</h1>
+        <h1 className='hero-title1'>LOGO</h1>
+      </div>
+      <div class="color-side">
+       
+      <LogInForm></LogInForm>
+      </div>    
+      </div>
+  </div>
   )
 }
 
-export default singIn
-
-
-
-
+export default SingInPage
