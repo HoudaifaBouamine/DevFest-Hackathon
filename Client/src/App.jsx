68634ef6c66726509bd151f570c32c7f274f1bd0
Aminemazari <mazariamine095@gmail.com>
2023-12-16 16:46:05 +0100

    i fuking fix all the problem mother fuker

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom" 
-import SingIn from "./pages/singIn/singInPage.jsx"
-import SingForAssociation from "./pages/singupPage/singForAssocaition.jsx"
-import SingForMember from "./pages/singupPage/singForMember.jsx"
-import Home from './pages/Homepage/home.jsx'
-import Projects from './pages/projects/project.jsx'
-import ChooseIdantity from "./pages/singupPage/chooseIdantity.jsx"
+
 import LandingPage from "./pages/landingPage/landingPage.jsx"
 import Pageone from "./pages/landingPage/pageone.jsx"
 
@@ -14,12 +8,7 @@ import Pagetwo from "./pages/landingPage/pagetwo.jsx"
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />}></Route>
-      <Route path="/home" element={<Home />}></Route>
-    </Routes>
-    </BrowserRouter>
+<LandingPage/>
   )
 }
 
